78c53e57f5d8f4f91a1d3beafb902fe8085db0cb
Yevanis Garcia <yevanis.garcia@gmail.com>
2018-09-10 23:28:29 -0400

    First commit 09/10/18

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, number of times is: {numTimes}");
+        }
+    }
+}
